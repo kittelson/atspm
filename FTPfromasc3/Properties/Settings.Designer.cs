27c18a9f22e8b7b0de6e2e84fe71d0352480d4b6
ian-cameron <ian@digiwest.com>
2017-09-18 23:31:16 -0700

    Settings

--- FTPfromasc3/Properties/Settings.Designer.cs
+++ FTPfromasc3/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace FTPfromAllControllers.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.1.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -182,6 +182,17 @@ namespace FTPfromAllControllers.Properties {
         public bool DealWithMoab {
             get {
                 return ((bool)(this["DealWithMoab"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=MOE;Initial Catalog=MOE;Persist Security Info=True;User ID=SPM;Passwo" +
+            "rd=CHANGEME")]
+        public string SPM {
+            get {
+                return ((string)(this["SPM"]));
             }
         }
     }
